420b67a087e8ecc44d506bdc7a28b2f6b3b037d3
Jerry <blawhi2435@gmail.com>
2024-02-09 03:16:58 +0800

    feat: add custom color

---
+++ Shanjuku/UI/Component/CustomColor.swift
@@ -0,0 +1,18 @@
+//
+//  CustomColor.swift
+//  Shanjuku
+//
+//  Created by 張家睿 on 2024/2/8.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let lightGreen: Color = Color("ShanjukuLightGreen")
+    static let darkGreen: Color = Color("ShanjukuDarkGreen")
+    static let lightBrown: Color = Color("ShanjukuLightBrown")
+    static let darkBrown: Color = Color("ShanjukuDarkBrown")
+    static let lightGray: Color = Color("ShanjukuLightGray")
+}
